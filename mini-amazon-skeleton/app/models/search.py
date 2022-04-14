b07f9b6d0614fe29d120e853f29c0495c65bb228
Kaichao Chang <kaichao.chang@duke.edu>
2022-04-14 05:20:16 -0400

    finish the selling order search function

--- mini-amazon-skeleton/app/models/search.py
+++ mini-amazon-skeleton/app/models/search.py
@@ -46,3 +46,64 @@ AND available = True
                         sql += "ORDER BY price DESC"
                 print(sql)
                 return app.db.execute(sql)
+
+class Order_Search:
+        def __init__(self, id, uid, buyer_fname, buyer_lname, buyer_addr, pid, seller_id, quantity, fulfilled_by_seller, time_purchased, p_name, price, time_fulfilled):
+                self.id = id
+                self.uid = uid
+                self.buyer_fname = buyer_fname
+                self.buyer_lname = buyer_lname
+                self.buyer_addr = buyer_addr
+                self.pid = pid
+                self.seller_id = seller_id
+                self.quantity = quantity
+                self.fulfilled_by_seller = fulfilled_by_seller
+                self.time_purchased = time_purchased
+                self.p_name = p_name
+                self.price = price 
+                self.time_fulfilled = time_fulfilled
+
+        @staticmethod
+        def search_order(uid, search_msg,  fulfill):
+                sql = """
+SELECT Purchases.id, Users.id, Users.firstname, Users.lastname, Users.address, Purchases.pid, Purchases.seller_id, Purchases.quantity, Purchases.fulfill_by_seller, Purchases.time_purchased, Products.name, Products.price * Purchases.quantity, Order_fulfill.time_fulfilled 
+From Purchases 
+LEFT JOIN Users  
+ON Purchases.uid = Users.id 
+LEFT JOIN Products 
+ON Purchases.pid = Products.id 
+LEFT JOIN Order_fulfill 
+ON Purchases.id = Order_fulfill.id 
+"""     
+                sql += \
+                        """
+WHERE ( CONCAT(Users.firstname, ' ', Users.lastname)  LIKE '%{}%' or Products.name LIKE '%{}%')
+                        """.format(search_msg, search_msg)
+                if fulfill == 0:
+                        sql += \
+                                """
+AND Purchases.seller_id = {}
+ORDER BY time_purchased DESC        
+                                """.format(uid)
+                
+                if fulfill == 1:
+                        sql += \
+                                """
+AND Purchases.seller_id = {}
+AND  Purchases.fulfill_by_seller = TRUE
+ORDER BY time_purchased DESC        
+                                """.format(uid)
+
+                if fulfill == 2:
+                        sql += \
+                                """
+AND Purchases.seller_id = {}
+AND  Purchases.fulfill_by_seller = FALSE
+ORDER BY time_purchased DESC        
+                                """.format(uid)
+                
+                rows = app.db.execute(sql)
+                return [Order_Search(*row) for row in rows]
+    
+               
+              
--- mini-amazon-skeleton/app/search.py
+++ mini-amazon-skeleton/app/search.py
@@ -5,7 +5,7 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_user, logout_user
 import datetime
 
-from app.models.search import Search
+from app.models.search import Search, Order_Search
 
 from .models.product import Product
 from .models.purchase import Purchase
@@ -41,3 +41,19 @@ def search():
         purchases = None
     return render_template('search_result.html', search_msg=search_msg, search_catagory=cate, orderby=order, match_products=product,
                            purchase_history=purchases,)
+
+@bp.route("/search_order", methods=['GET', "POST"])
+def search_order():
+    if Seller.is_seller(current_user.id):
+        current_user.is_current_seller = True    
+
+    search_msg = request.form.get('search-message')
+    fulfill = int(request.form.get('fulfill'))
+    
+    key_word = 0 
+    if len(search_msg) == 0:
+        key_word = 1
+    
+    search_order = Order_Search.search_order(current_user.id, search_msg, fulfill)
+
+    return render_template('order_search_result.html', search_order = search_order, search_msg = search_msg, fulfill = fulfill, key_word = key_word)
